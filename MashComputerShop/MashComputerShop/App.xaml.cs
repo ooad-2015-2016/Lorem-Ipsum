a7ce7d50ab7df3b7f1642fd357ca010c0b57c7da
Haris Imamovic <himamovic1@etf.unsa.ba>
2016-03-21 00:06:31 +0100

    Novi Windows Universal projekat.

--- MashComputerShop/MashComputerShop/App.xaml.cs
+++ MashComputerShop/MashComputerShop/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace MASH_ComputerShop
+namespace MashComputerShop
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- MashComputerShop/MashComputerShop/Properties/AssemblyInfo.cs
+++ MashComputerShop/MashComputerShop/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MASH-ComputerShop")]
+[assembly: AssemblyTitle("MashComputerShop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MASH-ComputerShop")]
+[assembly: AssemblyProduct("MashComputerShop")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
