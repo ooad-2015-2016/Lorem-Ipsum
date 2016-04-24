a946bd8833270f6942f4a2847821a2947654b049
Haris Imamovic <himamovic1@etf.unsa.ba>
2016-04-24 12:42:55 +0200

    Dodan model za klasu "RegisteredUser"

---
+++ MashComputerShop/MashComputerShop/MashShopDB/Models/MashShopDBContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace MashComputerShop.MashShopDB.Models
+{
+    public class MashShopDBContext : DbContext
+    {
+        // Set koji sadrzi sve registrovane korisnike
+        public DbSet<RegisteredUser> AllRegisteredUsers { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbFilePath = "MashShop.db";
+
+            try { dbFilePath = Path.Combine(ApplicationData.Current.LocalFolder.Path, dbFilePath); }
+            catch { /* Ovdje vjerovatno treba neki upis u log staviti */ }
+
+            // Određujemo koju bazu ćemo koristiti
+            optionsBuilder.UseSqlite($"Data source={dbFilePath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // postavljamo značenje "byte[]" u polju "ProfileImage"
+            modelBuilder.Entity<RegisteredUser>().Property(p => p.ProfileImage).HasColumnType("image");
+        }
+
+    }
+}
---
+++ MashComputerShop/MashComputerShop/MashShopDB/Models/RegisteredUser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Sqlite;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MashComputerShop.MashShopDB.Models
+{
+    public class RegisteredUser : User
+    {
+        // Polja klase odgovaraju poljima u tabeli u bazi padataka
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RegUserID { get { return UserID;  } set { UserID = value; } }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; } // let's pretend that this doesn't exist
+        public string Email { get; set; }
+        public string TelephoneNumber { get; set; }
+        public byte[] ProfileImage { get; set; }
+        
+
+        // Konstruktor
+        public RegisteredUser(int id = 0): base(id)
+        {
+            FirstName = "Jon"; LastName = "Doe";
+            Username = ""; Password = "";
+            Email = ""; TelephoneNumber = "";
+            ProfileImage = null;
+        }
+
+    }
+}
