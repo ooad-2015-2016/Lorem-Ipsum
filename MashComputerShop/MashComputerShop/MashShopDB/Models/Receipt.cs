5a12def9e1747ba518381e8114cfcef4742e5293
mirzaherdic <mirza.herdic@gmail.com>
2016-04-24 20:59:28 +0200

    Dodao Models za Receipt

---
+++ MashComputerShop/MashComputerShop/MashShopDB/Models/Receipt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MashComputerShop.MashDB.Models
+{
+    [Table("Receipts")]
+    class Receipt
+    {
+        [Key]
+        public int ReceiptID { get; set; }
+
+        public int CardID { get; set; }
+        public int CustomerID { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
